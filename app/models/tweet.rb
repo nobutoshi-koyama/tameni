a2bda625c198fea38b2b97280f7d2c6dbc1d096f
EC2 Default User <ec2-user@ip-172-31-31-175.us-east-2.compute.internal>
2019-03-11 10:35:47 +0000

    validates（一応完了)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,15 @@
 class Tweet < ApplicationRecord
     belongs_to :user
     
+    validates :menu, presence: true
+    validates :quantity, presence: true
+    validates :start_year, presence: true
+    validates :start_month, presence: true
+    validates :start_day, presence: true
+    validates :end_year, presence: true
+    validates :end_month, presence: true
+    validates :end_day, presence: true
+    
     def self.search(search)
       return Tweet.all unless search
       Tweet.where(['content LIKE ?', "%#{search}%"])
